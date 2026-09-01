d432feb87772eddd12614af1aa4dacf9eb4ac5aa
joao-vcthr <joaovicthorm1@gmail.com>
2026-08-31 22:30:00 -0300

    update: start corectrl minimized

--- autostart.lua
+++ autostart.lua
@@ -5,7 +5,7 @@
 hl.on("hyprland.start", function ()
   hl.exec_cmd("hyprpm reload -n")
   hl.exec_cmd("noctalia")
-  hl.exec_cmd("corectrl &")
+  hl.exec_cmd("corectrl --minimize-systray")
   hl.exec_cmd("systemctl --user start hyprpolkitagent")
   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
