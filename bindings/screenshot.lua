43db3529144104f2b54e85bcfa3e749aed87cccf
joao-vcthr <joaovicthorm1@gmail.com>
2026-08-31 22:30:51 -0300

    refactor

--- bindings/screenshot.lua
+++ bindings/screenshot.lua
@@ -4,12 +4,12 @@
 
 local mainMod = "SUPER"
 
-hl.bind("SUPER + SHIFT + S", hl.plugin.hyprcapture.open)
+hl.bind(mainMod .. " + SHIFT + S", hl.plugin.hyprcapture.open)
 
-hl.bind("SUPER + SHIFT + W", function()
+hl.bind(mainMod .. " + SHIFT + W", function()
     hl.plugin.hyprcapture.open("window")
 end)
 
-hl.bind("SUPER + SHIFT + F", function()
+hl.bind(mainMod .. " + SHIFT + F", function()
     hl.plugin.hyprcapture.open("fullscreen")
 end)
